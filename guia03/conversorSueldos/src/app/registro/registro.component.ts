1b63fbbc12e5480eccf8e9dfc73e5978458425c4
Napote <palenciabnathaly@gmail.com>
2020-08-28 11:42:34 -0600

    Validación de formulario con formControl

---
+++ guia03/conversorSueldos/src/app/registro/registro.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from 
+'@angular/forms';
+
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+
+
+export class RegistroComponent implements OnInit {
+  
+
+  readonly descuentoISSS = 0.073;
+  readonly descuentoAFP = 0.051;
+  readonly descuentoRenta = 0.11;
+
+
+  registro=[];
+  empleado:any;
+  nombreEmpleado:string;
+  salarioBase: number;
+  valorRenta:number;
+  valorISSS:number;
+  valorAFP:number;
+  salarioNeto: number
+
+  contador:number;
+
+  formIngreso: FormGroup;
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.nombreEmpleado="";
+    this.salarioBase=0;
+    this.valorRenta=0;
+    this.valorISSS=0;
+    this.valorAFP=0;
+    this.salarioNeto=0;
+    this.contador=0;
+
+    this.formIngreso = this.createFormGroup();
+
+  }
+
+  
+
+  ingresar(){
+    this.valorRenta = this.salarioBase*this.descuentoRenta;
+    this.valorISSS = +(this.salarioBase*this.descuentoISSS).toFixed(2);
+    this.valorAFP = this.salarioBase*this.descuentoAFP;
+    this.salarioNeto = this.salarioBase-(this.valorAFP+this.valorRenta+this.valorISSS);
+    this.empleado={"nombreEmpleado":this.nombreEmpleado,"salarioBase":this.salarioBase,"valorRenta":this.valorRenta,"valorAFP":this.valorAFP,"valorISSS":this.valorISSS,"salarioNeto":this.salarioNeto}; 
+    this.registro.push(this.empleado);
+    this.contador++;
+
+  }
+
+  createFormGroup(){
+    return new FormGroup({
+      txtNombre: new FormControl(''),
+      numSalarioBase: new FormControl('')
+    }); 
+  }
+
+  reset(){
+    this.formIngreso.reset();
+  }
+
+  
+
+}
