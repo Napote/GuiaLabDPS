f50cf5c7bf1bcc0b3d081d2b568b43754d7b4586
Napote <palenciabnathaly@gmail.com>
2020-08-27 18:14:10 -0600

    Agregado componente combo-box

--- guia03/Conversor/src/app/app.module.ts
+++ guia03/Conversor/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComboBoxComponent } from './combo-box/combo-box.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { ComboBoxComponent } from './combo-box/combo-box.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- guia03/Conversor/src/app/combo-box/combo-box.component.ts
+++ guia03/Conversor/src/app/combo-box/combo-box.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+//Agregar las siguientes modulos
+import {BrowserModule}from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-combo-box',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./combo-box.component.css']
 })
 export class ComboBoxComponent implements OnInit {
+  unidades;
+  opcionSeleccionado: string;
+  valorcm:number;
+  valorconversion:number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.unidades = ["Pulgada","Metro","Kilometro"]; 
+    this.opcionSeleccionado="Selecciona"; 
+    this.valorcm=0;
+    this.valorconversion=0;
   }
+
+  capturar(){
+    switch(this.opcionSeleccionado){ 
+      case'Pulgada':
+        this.valorconversion=this.valorcm* 0.39370; 
+      break;
+      case'Metro': 
+        this.valorconversion=this.valorcm/100;
+      break; 
+      case'Kilometro':
+        this.valorconversion=this.valorcm/100000; 
+      break;
+    }
+  }
+
+   
 
 }
