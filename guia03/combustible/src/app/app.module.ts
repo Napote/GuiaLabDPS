79934484ab365c2e8416409dcb705ee4d7f91a5a
shyfrost <shyfr0s7@gmail.com>
2020-08-28 22:36:59 -0600

    Agregado funcion para calcular total

--- guia03/combustible/src/app/app.module.ts
+++ guia03/combustible/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GasFormComponent } from './gas-form/gas-form.component';
@@ -13,7 +14,7 @@ import {FormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, FormsModule
+    AppRoutingModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- guia03/combustible/src/app/gas-form/gas-form.component.ts
+++ guia03/combustible/src/app/gas-form/gas-form.component.ts
@@ -1,26 +1,41 @@
 import { Component, OnInit } from '@angular/core';
- 
+import {BrowserModule}from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-gas-form',
   templateUrl: './gas-form.component.html',
   styleUrls: ['./gas-form.component.css']
 })
-export class GasFormComponent implements OnInit {
-  listaGasolina = [
-    {tipo: "Especial", precio: 4.25},
-    {tipo: "Regular", precio: 4.05},
-    {tipo: "Diesel", precio:3.96},     
-  ]; 
+export class GasFormComponent implements OnInit{
+
+  listaGasolina = []; 
 
   gasolinaSeleccionada: string;
   numeroGalones: number;
+  costoFinal: number;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.gasolinaSeleccionada="Especial";
-    this.numeroGalones=1;
+    this.listaGasolina = ["Especial","Regular","Diesel"]; 
+    this.gasolinaSeleccionada="Selecciona"; 
+    this.numeroGalones=0.5; 
   }
+
+  costoGalonIngresado(){
+    switch(this.gasolinaSeleccionada){ 
+      case'Especial':
+        this.costoFinal=this.numeroGalones*4.25; 
+      break;
+      case'Regular': 
+        this.costoFinal=this.numeroGalones*4.05;
+      break; 
+      case'Diesel':
+        this.costoFinal=this.numeroGalones*3.96; 
+      break;
+  }
+
+}  
 
 }
