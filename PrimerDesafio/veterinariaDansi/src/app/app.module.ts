198b60912256bcd924f5323b3fcfa6e704e0019f
palenciabnathaly@gmail.com <palenciabnathaly@gmail.com>
2020-08-29 18:42:43 -0600

    Agregado componente formulario-clientes

--- PrimerDesafio/veterinariaDansi/src/app/app.module.ts
+++ PrimerDesafio/veterinariaDansi/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioClientesComponent } from './formulario-clientes/formulario-clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioClientesComponent
   ],
   imports: [
     BrowserModule,
