97c0661b861dc65740906513e668d923c50dd966
palenciabnathaly@gmail.com <palenciabnathaly@gmail.com>
2020-09-27 23:14:07 -0600

    Primer proyecto con React

---
+++ guia07/pruebasreact/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import './App.css';
+
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Aquí haremos nuestro TO-DO list
+        </p>
+        <Form/>   
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
+ 
